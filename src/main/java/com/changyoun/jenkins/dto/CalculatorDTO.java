0ef293e392d2012c7b265b2b350cb3b91455f89b
leebackcoding <lcy2825@gmail.com>
2024-11-05 10:48:08 +0900

    jenkins files

---
+++ src/main/java/com/changyoun/jenkins/dto/CalculatorDTO.java
@@ -0,0 +1,16 @@
+package com.changyoun.jenkins.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CalculatorDTO {
+    @NonNull
+    private Integer num1;
+    @NonNull
+    private Integer num2;
+    private Integer sum;
+}
