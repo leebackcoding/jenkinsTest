0d93a62db171ce0dd62f613eefc4575f81be7d6c
leebackcoding <lcy2825@gmail.com>
2024-11-05 11:40:21 +0900

    dd

--- src/main/java/com/changyoun/jenkins/controller/CalculatorController.java
+++ src/main/java/com/changyoun/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive";
+        return "I'm alivesdfhkjasdhfsdahfskadjfl";
     }
 
     @GetMapping("/plus")
